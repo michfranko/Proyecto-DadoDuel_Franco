f8e1db5aa94224f15d259dab17def2cdf117fe4e
michfranko <jfrancon@est.ups.edu.ec>
2025-03-29 15:06:42 -0500

    cambio en funciomiento en html y javascript

--- javascript Funcionamiento .js
+++ javascript Funcionamiento .js
@@ -1,5 +1,7 @@
 let scores = [0, 0];
+let round = 1;
 let currentPlayer = 0;
+const maxRounds = 3;
 
 document.getElementById('rollButton').addEventListener('click', rollDice);
 document.getElementById('resetButton').addEventListener('click', resetGame);
@@ -10,25 +12,39 @@ function rollDice() {
     scores[currentPlayer] += diceValue;
     document.getElementById(`score${currentPlayer + 1}`).textContent = scores[currentPlayer];
 
+ if(round === maxRounds && currentPlayer === 1){
+    determineWinner();
+ }else {
     currentPlayer = currentPlayer === 0 ? 1 : 0;
-    if (scores[0] >= 20 || scores[1] >= 20) {
-        determineWinner();
-    }
+    if (currentPlayer === 0) round++;
+    document.getElementById('currentPlayer').textContent = `Jugador ${currentPlayer + 1}`;
+    document.getElementById('Round').textContent = round;
+  }
 }
 
 function determineWinner() {
     document.getElementById('rollButton').disabled = true;
-    let resultText = scores[0] > scores[1] ? '¡Jugador 1 gana!' : '¡Jugador 2 gana!';
-    document.getElementById('result').textContent = resultText;
+   let resultText = '';
+   if(scores[0] > scores[1]){
+    resultText = 'Jugador 1 gana !';
+   } else if (scores[0] < scores[1]){
+    resultText = 'Jugador 2 gana!';
+   }else {
+    resultText = '¡Empate!';
+   }
+   document.getElementById('result').textContent = resultText;
 }
 
 function resetGame() {
     scores = [0, 0];
+    round = 1;
     currentPlayer = 0;
     document.getElementById('dice1').textContent = '';
     document.getElementById('dice2').textContent = '';
     document.getElementById('score1').textContent = '0';
     document.getElementById('score2').textContent = '0';
+    document.getElementById('currentPlayer').textContent ='Jugador 1';
+    document.getElementById('round').textContent = '1';
     document.getElementById('result').textContent = '';
     document.getElementById('rollButton').disabled = false;
 }
